02316079291192376f0fd92ac0e297bf0c2da188
ソンダンク <74962930+Aruoto@users.noreply.github.com>
2021-09-13 10:09:46 +0530

    - some [changes] in alive

--- Cynics/plugins/alive.py
+++ Cynics/plugins/alive.py
@@ -1,30 +1,21 @@
 import time
 import os
 from io import BytesIO
+from datetime import datetime
 from platform import python_version
 from Cynics.setclient import CynicsCli
 from pyrogram import filters, __version__
 from pyrogram.types import Message
-from Cynics import PREFIX, OWNER_NAME, CYNICS_VERSION, UPSTREAM_REPO
+from Cynics import PREFIX, OWNER_NAME, CYNICS_VERSION, UPSTREAM_REPO, StartTime
 from pyrogram.raw.all import layer
 from Cynics.helpers.clear_strings import clear_string
-
-# -- Constants -- #
-ALIVE_TEXT = (
-    "<b>✨ [Cynics]({}):</b>\n"
-    "<b> - Owner:</b> `{}`\n"
-    "<b> - Pyrogram Version:</b> `{}`\n"
-    "<b> - Python Version:</b> `{}`\n"
-    "<b> - Cynics Version:</b> `{}`\n\n"
-)
-# -- Constants End -- #
 
 __PLUGIN__ = os.path.basename(__file__.replace(".py", ""))
 
 __help__ = f"""
 Basic Commands of Userbot!
 
-`{PREFIX}alive` / `{PREFIX}start`: Check if bot is alive or not.
+`{PREFIX}alive`/`{PREFIX}start`: Check if bot is alive or not.
 `{PREFIX}ping`: Get pinged.
 `{PREFIX}id`: Get the ID of the file/user/group.
 `{PREFIX}json`: Get json of the replied message.
@@ -35,16 +26,33 @@ Basic Commands of Userbot!
     filters.command(["alive", "start"], PREFIX) & filters.me
 )
 async def check_alive(c: CynicsCli, m: Message):
-    await m.edit_text(
-        ALIVE_TEXT.format(
-            UPSTREAM_REPO,
-            OWNER_NAME,
-            __version__,
-            python_version(),
-            CYNICS_VERSION,
-        ),
-        disable_web_page_preview=True,
-    )
+    alive_img = "https://telegra.ph/file/4718cbed7c63bd52031a4.jpg"
+    alive_str = f"""
+「**[Cynics-UserBot]({UPSTREAM_REPO})**」 **is Alive!!**
+**Owner**: `{OWNER_NAME}`
+**Version**: `{CYNICS_VERSION}`
+**PyroGram Version**: `{__version__}`
+**Uptime**: `{str(datetime.now() - StartTime).split('.')[0]}`
+**Python Version**: `3.9.6`
+"""    
+    await m.delete()
+    if m.reply_to_message:
+        await c.send_photo(
+            m.chat.id,
+            alive_img,
+            caption=alive_str,
+            reply_to_message_id=m.reply_to_message.message_id,
+        )
+    else:
+        await c.send_photo(m.chat.id, alive_img, caption=alive_str)
+
+
+@CynicsCli.on_message(
+    filters.command("repo", PREFIX) & filters.me
+)
+async def repo(c: CynicsCli, message: Message):
+    repo = "Official Source Code Of [Cynics](https://github.com/CandyGang/Cynics.git)"
+    await message.edit_text(repo, disable_web_page_preview=True)
 
 
 @CynicsCli.on_message(filters.command("id", PREFIX) & filters.me)
@@ -116,7 +124,7 @@ async def jsonify(c: CynicsCli, m: Message):
     else:
         the_real_message = m
     try:
-        await m.reply_text(f"<code>{the_real_message}</code>")
+        await m.edit_text(f"<code>{the_real_message}</code>")
     except Exception as e:
         OUTPUT = clear_string(the_real_message)  # Remove the html elements using regex
         with BytesIO(str.encode(OUTPUT)) as f:
@@ -124,3 +132,14 @@ async def jsonify(c: CynicsCli, m: Message):
             await m.reply_document(document=f, caption=str(e))
         await m.delete()
     return
+
+@CynicsCli.on_message(filters.command("save", PREFIX) & filters.me)
+async def to_saved(c: CynicsCli, message: Message):
+    await message.reply_to_message.forward("self")
+    await message.edit_text('`Saved message.`')
+
+@CynicsCli.on_message(filters.command(["uptime", "up"], PREFIX) & filters.me)
+async def uptime(c: CynicsCli, message: Message):
+    uptime = f"**Current Uptime:** `{str(datetime.now() - StartTime).split('.')[0]}`"
+    await message.edit_text(uptime)
+
